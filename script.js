a9031d7ac35195d14953a23073b4124b0ec09d44
arminaparaty <armin5.amir.1391@gmail.com>
2026-07-17 17:17:27 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('CheatPhantom');
+// CheatPhantom
+
+console.log("CheatPhantom Loaded");
+
+// Button Animation
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(btn=>{
+
+btn.addEventListener("mouseenter",()=>{
+
+btn.style.transform="scale(1.04)";
+
+});
+
+btn.addEventListener("mouseleave",()=>{
+
+btn.style.transform="scale(1)";
+
+});
+
+});
+
+// Fade Animation
+
+window.onload=()=>{
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+
+document.body.style.transition="0.6s";
+
+document.body.style.opacity="1";
+
+},100);
+
+};
